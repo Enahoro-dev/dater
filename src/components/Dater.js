d17532f24f3fb35bd5a7954e0245b3b3a95cb79e
Abhulimen Enahoro <abhulimenenahoro@gmail.com>
2022-04-26 07:23:09 +0100

    Update Dater.js

--- src/components/Dater.js
+++ src/components/Dater.js
@@ -16,7 +16,7 @@ const Dater = () => {
         <div>
             <div className='flex flex-col justify-center items-center'>
                 <label className='text-white mb-1 text-sm'>LAST MENSTRUAL PERIOD</label>
-                <DatePicker  minDate={minDate} maxDate={dateMax} onChange={setDate} value={date} className='date-picker'/>
+                <DatePicker dayPlaceholder='dd' monthPlaceholder='mm' yearPlaceholder='yyyy' minDate={minDate} maxDate={dateMax} onChange={setDate} value={date} className='date-picker'/>
             </div>
             <div  className='text-white ml-0 mt-8 text-sm flex flex-col items-center'>
                 <div className='flex flex-col items-start'>
