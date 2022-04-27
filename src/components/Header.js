9c249bf68cf28e253326d84b296f79c3810e3a90
Abhulimen Enahoro <abhulimenenahoro@gmail.com>
2022-04-27 04:45:06 +0100

    added accents

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return(
         <div className='flex flex-col justify-center items-center'>
             <div>
-                <h1 className='text-lg text-white mb-2 tracking-wide font-bold'>CALCULER LE BEBE</h1>
+                <h1 className='text-lg text-white mb-2 tracking-wide font-bold'>CALCULER LE BÉBÉ</h1>
             </div>
             <div>
                 <img src={clbImage} alt='clb'/>
