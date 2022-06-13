a898a358f0c27efcec312913897ece7803b702f3
Abhulimen Enahoro <abhulimenenahoro@gmail.com>
2022-06-13 08:52:38 +0100

    Update Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,13 +4,13 @@ import ConvertEdd from './ConvertEdd.js';
 import ConvertGa from './ConvertGa.js';
 
 const Popup = (props) => {
-let popupString = props.popup? 'h-0 transition-all ' : 'h-2/5 transition-all duration-400 ease-in-out'
+let popupString = props.popup? 'h-0 transition-all ' : 'h-1/2 transition-all duration-400 ease-in-out'
 
 return (
         <div className={`text-green bottom-0 fixed text-lg font-bold bg-white z-10 w-full rounded-t-3xl ${popupString}` }>
             <div className='w-1/5 border-t border-green mx-auto border-4 mt-2'></div>
             <h2 className='text-green flex flex-col items-center mt-4 text-xl'>RESULTS</h2>
-            <div className='flex flex-col items-start mt-6 ml-12'>
+            <div className='flex flex-col items-start mt-4 ml-12'>
                 <ul className='list-disc text-green'>
                     <li><ConvertEdd date={props.date}/></li>
                     <li><ConvertGa date={props.date}/></li>
